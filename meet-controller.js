35feb141902492c575c0396909777e76e01373e2
alex <alex@pdp7.net>
2021-11-20 14:11:31 +0100

    Add feedback for mute actions

--- meet-controller.js
+++ meet-controller.js
@@ -3,12 +3,14 @@ var meetTabId = null;
 function tabUpdateListener(tabId, changeInfo, tab) {
     if(changeInfo.status == "complete") {
         meetTabId = tabId;
+        browser.browserAction.setBadgeText({text: "AV"});
         browser.browserAction.enable();
     }
 }
 
 function tabRemoveListener(tabId, removeInfo) {
     if(tabId == meetTabId) {
+        browser.browserAction.setBadgeText({text: ""});
         browser.browserAction.disable();
     }
 }
--- popup.js
+++ popup.js
@@ -1,8 +1,28 @@
 const toggle_audio = "window.document.dispatchEvent(new KeyboardEvent('keydown',{'key':'d', 'ctrlKey': true}));"
 const toggle_video = "window.document.dispatchEvent(new KeyboardEvent('keydown',{'key':'e', 'ctrlKey': true}));"
 
+var audio = true;
+var video = true;
+
 document.querySelectorAll("button").forEach(e => e.onclick = function(e) {
     var script = e.target.id === "video" ? toggle_video : toggle_audio;
+
+    if(e.target.id == "audio") {
+        audio = !audio;
+        document.querySelector("#audio").innerHTML = audio ? "Mute audio" : "Unmute audio";
+    }
+
+    if(e.target.id == "video") {
+        video = !video;
+        document.querySelector("#video").innerHTML = audio ? "Mute video" : "Unmute video";
+    }
+
+    var badge = "";
+    badge += audio ? "A" : "a";
+    badge += video ? "V" : "v";
+
+    browser.browserAction.setBadgeText({text: badge});
+
     browser.tabs.query({url: "*://meet.google.com/*-*-*"}).then(tabs => {
         for(var tab of tabs) {
             browser.tabs.executeScript(tab.id, {code: script}).then();
