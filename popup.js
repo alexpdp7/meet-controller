77afd13e66673938200eb6e6cc1b0dca6725433c
alex <alex@pdp7.net>
2021-11-20 13:01:55 +0100

    Add mute buttons

---
+++ popup.js
@@ -0,0 +1,15 @@
+const toggle_audio = "window.document.dispatchEvent(new KeyboardEvent('keydown',{'key':'d', 'ctrlKey': true}));"
+const toggle_video = "window.document.dispatchEvent(new KeyboardEvent('keydown',{'key':'e', 'ctrlKey': true}));"
+
+
+
+document.querySelectorAll("button").forEach(e => e.onclick = function(e) {
+    var script = e.target.id === "video" ? toggle_video : toggle_audio;
+    console.log(e.target.id);
+    console.log(script);
+    browser.tabs.query({url: "*://meet.google.com/*-*-*"}).then(tabs => {
+        for(var tab of tabs) {
+            browser.tabs.executeScript(tab.id, {code: script}).then();
+        }
+    }, console.log);
+});
